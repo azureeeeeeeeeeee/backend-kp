fcff56913b4497a4625c037e3b85c6e26f178f50
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2025-10-08 13:12:34 +0800

    fix: fixing column dtype

--- database/migrations/2025_08_10_123949_gallery_media.php
+++ database/migrations/2025_08_10_123949_gallery_media.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
 
             $table->foreignId('gallery_id')->constrained()->onDelete('cascade');
-            $table->date('path');
+            $table->string('path');
 
             $table->timestamps();
         });
